cd0960a22e1d089882ac0a5c49a51c2fd715d3a4
eduardogiorgio <eduardo.giorgio69@gmail.com>
2015-08-02 18:01:33 -0300

    correcciones

---
+++ RPG-blender-tactics/Assets/coinTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Hello", gameObject);
+		Destroy(other.gameObject);
+
+	}
+
+	void OnTriggerStay(Collider other) {
+		Debug.Log ("Hello", gameObject);
+
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("Hello", gameObject);
+
+	}
+}
