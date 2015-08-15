e93fb1c2f09a278842ea14946ce3741a1afca3bb
eduardogiorgio <eduardo.giorgio69@gmail.com>
2015-08-15 14:35:06 -0300

    commit test

---
+++ RPG-blender-tactics/Assets/count_coins.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class count_coins : MonoBehaviour {
+	private  Text Punctuation;
+	private static int coins = 0;
+
+	// Use this for initialization
+	void Start () {
+		GameObject PunctuationGo = GameObject.Find("Punctuation");
+		Punctuation = PunctuationGo.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(coins == 10){
+			Application.Quit();
+		}
+		Punctuation.text = "coins: " + coins.ToString ();
+	}
+
+	public static void updateCoins(){
+		coins++;
+		Debug.Log (coins.ToString());
+
+	}
+
+}
---
+++ RPG-blender-tactics/Assets/intercative.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class intercative : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider){
+		
+		if(collider.gameObject.tag == "Player")
+		{
+			Debug.Log("Interacted!");
+		}
+		
+	}
+}
